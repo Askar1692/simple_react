997ce6ba41412d7130bf2d8db6a8e4ec34120c33
Askar1692 <askar.kusemisov@gmail.com>
2022-07-19 23:34:09 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
+import {OnOff} from "./components/onOff/onOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontroledRating} from "./components/UncontroledRating/UncontroledRating";
+import {UncontrolledOnOff} from "./components/UncontrolledOnOff/uncontrolledOnOff";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props: any) {
+    console.log("App rendering")
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    let [switchOn, setSwitchOn] = useState<boolean>(false)
+
+    return (
+        <div className={'App'}>
+            {/*<OnOff on={switchOn} onChange={setSwitchOn}/>*/}
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            {/*<UncontrolledAccordion titleValue={"Menu"}/>*/}
+            <UncontroledRating/>
+
+            <Accordion titleValue={"Menu"}
+                       collapsed={accordionCollapsed}
+                       onChange={() => {
+                           setAccordionCollapsed(!accordionCollapsed)
+                       }}/>
+            <UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}
+
+            {/* <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>*/}
+
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return (
+        <div>
+            <h1> {props.title}</h1>
+        </div>
+    );
+}
+
 
 export default App;
